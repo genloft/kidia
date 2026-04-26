b5f52889581dc63b1cbfc35b99bba2ba13edfb07
pro0 <garciagarcia.juanantonio@gmail.com>
2026-04-26 20:12:39 +0200

    Switch to @astrojs/node for Hostinger support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: vercel(),
+    adapter: node({
+      mode: 'standalone'
+    }),
     server: {
         port: 3000
     }
