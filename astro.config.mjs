13d0325f4102246150fb97423db4ef1e67f9422f
pro0 <garciagarcia.juanantonio@gmail.com>
2026-05-18 21:26:34 +0200

    Revert to static output to fix Hostinger 403

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,6 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: node({
-        mode: 'standalone'
-    }),
     server: {
         port: 3000
     },
